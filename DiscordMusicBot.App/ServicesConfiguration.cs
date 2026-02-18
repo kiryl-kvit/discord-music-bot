a01d821372c258e56f632fe8de70734e4c6ed17a
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-02-18 18:13:27 +0100

    refactor DI configuration

--- DiscordMusicBot.App/ServicesConfiguration.cs
+++ DiscordMusicBot.App/ServicesConfiguration.cs
@@ -21,64 +21,87 @@ public static class ServicesConfiguration
 {
     public static IServiceCollection ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
-        services.Configure<BotSettings>(configuration.GetSection(BotSettings.SectionName));
-        services.Configure<SqliteDatabaseOptions>(configuration.GetSection(SqliteDatabaseOptions.SectionName));
-
-        services.AddOptions<SqliteDatabaseOptions>()
-            .Bind(configuration.GetSection(SqliteDatabaseOptions.SectionName))
-            .Validate(o => !string.IsNullOrWhiteSpace(o.DbFilePath), "SqliteDatabase:DbFilePath is required.")
-            .ValidateOnStart();
-        
-        services.AddOptions<BotSettings>()
-            .Bind(configuration.GetSection(BotSettings.SectionName))
-            .Validate(o => !string.IsNullOrWhiteSpace(o.BotToken), "BotSettings:BotToken is required.")
-            .Validate(o => !string.IsNullOrWhiteSpace(o.AppId), "BotSettings:AppId is required.")
-            .Validate(o => !string.IsNullOrWhiteSpace(o.PublicKey), "BotSettings:PublicKey is required.")
-            .ValidateOnStart();
-
-        services.AddDbContext<MusicBotDbContext>((sp, options) =>
-        {
-            var dbOptions = sp.GetRequiredService<IOptions<SqliteDatabaseOptions>>().Value;
-            options.UseSqlite($"Data Source={dbOptions.DbFilePath}");
-        });
-
-        services.AddHostedService<DbInitializerHostedService>();
-        services.AddScoped<IPlayQueueRepository, PlayQueueRepository>();
+        services.ConfigureBotOptions(configuration)
+            .ConfigureDatabase(configuration)
+            .ConfigureDiscordDotNet();
 
         services.AddKeyedScoped<IUrlProcessor, YoutubeUrlProcessor>(SupportedSources.YoutubeKey);
         services.AddScoped<IUrlProcessorFactory, UrlProcessorFactory>();
 
-        var socketConfig = new DiscordSocketConfig
-        {
-            GatewayIntents = GatewayIntents.AllUnprivileged,
-            LogLevel = LogSeverity.Info,
-        };
-
-        services.AddSingleton(socketConfig);
-        services.AddSingleton<DiscordSocketClient>(sp =>
-        {
-            var logger = sp.GetRequiredService<ILogger<DiscordSocketClient>>();
-            var client = new DiscordSocketClient(socketConfig);
-
-            client.Log += Logging.CreateLogHandler(logger);
-
-            return client;
-        });
-
-        services.AddSingleton<InteractionService>(sp =>
-        {
-            var client = sp.GetRequiredService<DiscordSocketClient>();
-            var logger = sp.GetRequiredService<ILogger<InteractionService>>();
-            
-            var interactionService = new InteractionService(client);
-
-            interactionService.Log += Logging.CreateLogHandler(logger);
-
-            return interactionService;
-        });
-
-        services.AddSingleton<InteractionHandler>();
-
         return services;
+    }
+
+    extension(IServiceCollection services)
+    {
+        private IServiceCollection ConfigureBotOptions(IConfiguration configuration)
+        {
+            services.Configure<BotSettings>(configuration.GetSection(BotSettings.SectionName));
+
+            services.AddOptions<BotSettings>()
+                .Bind(configuration.GetSection(BotSettings.SectionName))
+                .Validate(o => !string.IsNullOrWhiteSpace(o.BotToken), "BotSettings:BotToken is required.")
+                .Validate(o => !string.IsNullOrWhiteSpace(o.AppId), "BotSettings:AppId is required.")
+                .Validate(o => !string.IsNullOrWhiteSpace(o.PublicKey), "BotSettings:PublicKey is required.")
+                .ValidateOnStart();
+
+            return services;
+        }
+
+        private IServiceCollection ConfigureDatabase(IConfiguration configuration)
+        {
+            services.Configure<SqliteDatabaseOptions>(configuration.GetSection(SqliteDatabaseOptions.SectionName));
+
+            services.AddOptions<SqliteDatabaseOptions>()
+                .Bind(configuration.GetSection(SqliteDatabaseOptions.SectionName))
+                .Validate(o => !string.IsNullOrWhiteSpace(o.DbFilePath), "SqliteDatabase:DbFilePath is required.")
+                .ValidateOnStart();
+
+            services.AddDbContext<MusicBotDbContext>((sp, options) =>
+            {
+                var dbOptions = sp.GetRequiredService<IOptions<SqliteDatabaseOptions>>().Value;
+                options.UseSqlite($"Data Source={dbOptions.DbFilePath}");
+            });
+
+            services.AddHostedService<DbInitializerHostedService>();
+            services.AddScoped<IPlayQueueRepository, PlayQueueRepository>();
+            return services;
+        }
+
+        private IServiceCollection ConfigureDiscordDotNet()
+        {
+            var socketConfig = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildVoiceStates |
+                                 GatewayIntents.Guilds,
+                LogLevel = LogSeverity.Info,
+            };
+
+            services.AddSingleton(socketConfig);
+            services.AddSingleton<DiscordSocketClient>(sp =>
+            {
+                var logger = sp.GetRequiredService<ILogger<DiscordSocketClient>>();
+                var client = new DiscordSocketClient(socketConfig);
+
+                client.Log += Logging.CreateLogHandler(logger);
+
+                return client;
+            });
+
+            services.AddSingleton<InteractionService>(sp =>
+            {
+                var client = sp.GetRequiredService<DiscordSocketClient>();
+                var logger = sp.GetRequiredService<ILogger<InteractionService>>();
+
+                var interactionService = new InteractionService(client);
+
+                interactionService.Log += Logging.CreateLogHandler(logger);
+
+                return interactionService;
+            });
+
+            services.AddSingleton<InteractionHandler>();
+
+            return services;
+        }
     }
 }
