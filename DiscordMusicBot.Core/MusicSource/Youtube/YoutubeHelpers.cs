d544914f7ee29a1a42615e0bcebfa320de6095e8
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-02-22 18:56:42 +0100

    replace abstract with static

--- DiscordMusicBot.Core/MusicSource/Youtube/YoutubeHelpers.cs
+++ DiscordMusicBot.Core/MusicSource/Youtube/YoutubeHelpers.cs
@@ -2,7 +2,7 @@ using YoutubeExplode.Videos;
 
 namespace DiscordMusicBot.Core.MusicSource.Youtube;
 
-public abstract class YoutubeHelpers
+public static class YoutubeHelpers
 {
     public static string VideoUrl(VideoId videoId) => $"https://www.youtube.com/watch?v={videoId}";
 }
